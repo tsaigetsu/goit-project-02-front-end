df3d74832207d2c9e7a915730376d054413f9873
Alina <alia.nord.adventurer@gmail.com>
2024-11-01 16:45:50 +0100

    routing App jsx & main jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-// import { useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Sidebar from "./components/Sidebar/Sidebar";
+import WelcomePage from "./pages/WelcomePage/WelcomePage";
+import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
+import PublicRoute from "./Routes/PublicRoute";
+import PrivateRoute from "./Routes/PrivateRoute";
+import HomePage from "./pages/HomePage/HomePage";
+import ScreensPage from "./pages/ScreensPage/ScreensPage";
 
 function App() {
-  return <Sidebar />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/welcome" replace />} />
+
+      <Route
+        path="/welcome"
+        element={<PublicRoute component={<WelcomePage />} redirectTo="/home" />}
+      />
+
+      {/* <Route
+        path="/auth/:id"
+        element={
+          <PublicRoute
+            component={<AuthPage />}
+            redirectTo="/home"
+          />
+        }
+      >
+        <Route path="login" element={<LoginForm />} />
+        <Route path="register" element={<RegisterForm />} />
+      </Route> */}
+
+      <Route
+        path="/home"
+        element={
+          <PrivateRoute component={<HomePage />} redirectTo="/welcome" />
+        }
+      >
+        <Route path=":boardId" element={<ScreensPage />} />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/Routes/PublicRoute.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../redux/auth/selectors";
+import { Navigate } from "react-router-dom";
+
+const PublicRoute = ({ component: Component, redirectTo = "/" }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn); // проверить селектор
+  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+};
+
+export default PublicRoute;
