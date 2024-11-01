00428368afb5906fe6afcd120a7a5d4a720655cd
Alina <alia.nord.adventurer@gmail.com>
2024-11-01 12:10:54 +0100

    sidebar board list & item

--- src/components/SidebarBoardList/SidebarBoardList.jsx
+++ src/components/SidebarBoardList/SidebarBoardList.jsx
@@ -1,7 +1,14 @@
+import { useDispatch, useSelector } from "react-redux";
 import SidebarBoardItem from "../SidebarBoardItem/SidebarBoardItem";
 import s from "./SidebarBoardList.module.css";
+import { useEffect } from "react";
 
 const SidebarBoardList = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBoardsThunk());
+  }, [dispatch]);
+  const data = useSelector((state) => selectBoards(state));
   return (
     <ul className={s.ul}>
       {/* нужен скролл */}
@@ -10,8 +17,9 @@ const SidebarBoardList = () => {
         <p className={s.createBoardText}>Create a new board</p>
         <button className={s.createBoardBtn}>+</button>
       </li>
-      <SidebarBoardItem /> {/* в подальшому метод map */}
-      <SidebarBoardItem />
+      {data.map((item) => (
+        <SidebarBoardItem key={item.id} name={item.name} id={item.id} />
+      ))}
     </ul>
   );
 };
