d3ab636ac1616ec85915622c609c324d1d204a91
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-11-03 16:34:05 +0100

    add addColumn UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
-import { useState } from "react";
-import viteLogo from "/favicon.svg";
-
-// import CardManager from "./components/CardManager/CardManager.jsx";
-
+// import { useState } from "react";
+// import viteLogo from "/favicon.svg";
 import "./App.css";
-import EditProfile from "./components/EditProfile/Editprofile";
-import LoginForm from "./components/LoginForm/LoginForm";
+
+// import EditProfile from "./components/EditProfile/Editprofile";
+// import AddColumn from "./components/AddColumn/AddColumn";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <LoginForm />
-        <EditProfile />
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{/* <AddColumn /> */}</div>
     </>
   );
 }
