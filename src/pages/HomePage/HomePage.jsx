10d8e2aedf0070cd6e4339169736933529d9288b
KotyshevaYulya <yulyaxd07@gmail.com>
2024-11-05 23:24:58 +0100

    add header

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,20 @@
 import Sidebar from "../../components/Sidebar/Sidebar";
 import ScreensPage from "../ScreensPage/ScreensPage";
+import Header from "../../components/Header/Header.jsx";
 import css from "./HomePage.module.css";
+import { SidebarContext } from "../../components/Layout/Layout.jsx";
+import React, { useState, createContext, useContext } from 'react'; 
+import Layout from "../../components/Layout/Layout.jsx"; // Імпортуйте Layout
+
 const HomePage = () => {
+  const { isSidebarOpen } = useContext(SidebarContext);
+  console.log(isSidebarOpen);
+
   return (
-    <>
-      <section className={css.wrapperHomePage}>
-        <Sidebar />
-        <ScreensPage />
-      </section>
-    </>
+    <Layout>
+      <Sidebar className={isSidebarOpen ? "aside open" : "aside"} />
+      <ScreensPage />
+    </Layout>
   );
 };
 
