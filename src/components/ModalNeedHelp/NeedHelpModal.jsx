7c6758dcf05feb0e0021143d34f7cba2819dc3dc
Oleksii94 <113771217+annabusaga@users.noreply.github.com>
2024-11-06 21:13:26 +0100

    need help

---
+++ src/components/ModalNeedHelp/NeedHelpModal.jsx
@@ -0,0 +1,23 @@
+import Modal from "../Modal/Modal";
+import NeedHelpForm from "./NeedHelpForm";
+
+const NeedHelpModal = ({ closeModal }) => {
+  const handleCloseModal = () => {
+    closeModal();
+  };
+
+  return (
+    <>
+      <Modal
+        isOpen={true}
+        handleClose={handleCloseModal}
+        heading={"Need help"}
+        modalType={"modalHelp"}
+      >
+        <NeedHelpForm handleCloseModal={handleCloseModal} />
+      </Modal>
+    </>
+  );
+};
+
+export default NeedHelpModal;
