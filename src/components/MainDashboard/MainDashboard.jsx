430b5b68693e0c836e14b1546c02546c25617b71
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-11-07 12:10:33 +0100

    fix screenpage

--- src/components/MainDashboard/MainDashboard.jsx
+++ src/components/MainDashboard/MainDashboard.jsx
@@ -1,17 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import css from "./MainDashboard.module.css";
 import ColumnsList from "../ColumnsList/ColumnsList";
 import { useDispatch, useSelector } from "react-redux";
 import { selectError, selectLoading } from "../../redux/columns/slice";
-import NewBoardForm from "../NewBoardForm/NewBoardForm";
-import { selectBoards } from "../../redux/boards/selectors";
+
 import { fetchBoardsThunk } from "../../redux/boards/operations";
 
 const MainDashboard = ({ board }) => {
-  const boards = useSelector(selectBoards);
+  // const boards = useSelector(selectBoards);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
-  const [isOpen, setIsOpen] = useState(false);
+
   const dispatch = useDispatch();
   console.log("bardId", board.id);
 
@@ -24,39 +23,13 @@ const MainDashboard = ({ board }) => {
   if (loading) return <p>Loading columns...</p>;
   if (error) return <p>Error loading columns: {error}</p>;
 
-  const onOpen = () => {
-    setIsOpen(true);
-  };
-
-  const onClose = () => {
-    setIsOpen(false);
-  };
   return (
     <>
       <div className={css.wrapperMainDashboard}>
         <div className={css.wrapperText}>
-          {boards.length === 0 ? (
-            <p className={css.text}>
-              Before starting your project, it is essential
-              <span>
-                <button className={css.spanBtn} onClick={onOpen}>
-                  to create a board
-                </button>
-              </span>
-              to visualize and track all the necessary tasks and milestones.
-              This board serves as a powerful tool to organize the workflow and
-              ensure effective collaboration among team members.
-            </p>
-          ) : (
-            // <div className={css.wrapperText}>
-            // </div>
-            <div className={css.columnsWrapper}>
-              <ColumnsList boardId={board.id} />
-            </div>
-          )}
-          {isOpen && (
-            <NewBoardForm isOpen={isOpen} onClose={onClose} onSave={onOpen} />
-          )}
+          <div className={css.columnsWrapper}>
+            <ColumnsList boardId={board.id} />
+          </div>
         </div>
       </div>
     </>
--- src/pages/ScreensPage/ScreensPage.jsx
+++ src/pages/ScreensPage/ScreensPage.jsx
@@ -1,22 +1,46 @@
 import css from "./ScreensPage.module.css";
-
+import { selectBoards } from "../../redux/boards/selectors";
+import { useSelector, useState } from "react";
 import HeaderDashboard from "../../components/HeaderDashboard/HeaderDashboard.jsx";
 import MainDashboard from "../../components/MainDashboard/MainDashboard.jsx";
 
 const ScreensPage = ({ board }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  // const board = useSelector(selectBoards);
   console.log("board", board);
+  const onOpen = () => {
+    setIsOpen(true);
+  };
 
+  const onClose = () => {
+    setIsOpen(false);
+  };
   return (
     <>
       <section className={css.wrapperScreenPage}>
-        <div className={css.screensPage__content}>
-          <HeaderDashboard
-            titleBoard={board.title}
-            className={css.headerDashboard}
-          />
-          <MainDashboard board={board} className={css.mainDashboard} />
-        </div>
+        {boards.length === 0 ? (
+          <p className={css.text}>
+            Before starting your project, it is essential
+            <span>
+              <button className={css.spanBtn}>to create a board</button>
+            </span>
+            to visualize and track all the necessary tasks and milestones. This
+            board serves as a powerful tool to organize the workflow and ensure
+            effective collaboration among team members.
+          </p>
+        ) : (
+          <div className={css.screensPage__content}>
+            <HeaderDashboard
+              titleBoard={board.title}
+              className={css.headerDashboard}
+            />
+            <MainDashboard board={board} className={css.mainDashboard} />
+          </div>
+        )}
       </section>
+      {isOpen && (
+        <NewBoardForm isOpen={isOpen} onClose={onClose} onSave={onOpen} />
+      )}
     </>
   );
 };
