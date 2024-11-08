4ecc65065558975d0df3d783ef70f3f84666e8be
Alina <alia.nord.adventurer@gmail.com>
2024-11-08 14:19:54 +0100

    bugs fixed

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -21,7 +21,7 @@ const validateFormSchema = Yup.object().shape({
   email: Yup.string()
     .required("Email is required")
     .matches(
-      /^[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+      /^[A-Za-z0-9!#$%&'.*+/=?^_`{|}~-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
       "Invalid email format"
     ),
   password: Yup.string()
