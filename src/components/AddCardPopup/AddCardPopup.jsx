79e9f1af0ed91b1e8f767ad71d80a487260b8f91
Olha Matviienko <olmatv09@gmail.com>
2024-11-09 01:25:28 +0100

    fixed add/edit popup

--- src/components/AddCardPopup/AddCardPopup.jsx
+++ src/components/AddCardPopup/AddCardPopup.jsx
@@ -5,7 +5,7 @@ import SvgIcon from '../SvgIcon/SvgIcon';
 import CalendarPicker from '../CalendarPicker/CalendarPicker';
 import s from './AddCardPopup.module.css';
 
-const AddCardPopup = ({ onClose, onAdd, userId, columnId, accessToken }) => {
+const AddCardPopup = ({ closeModal, onAdd, userId, columnId, accessToken }) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [labelColor, setLabelColor] = useState('');
@@ -39,7 +39,7 @@ const AddCardPopup = ({ onClose, onAdd, userId, columnId, accessToken }) => {
 
       const data = await response.json();
       onAdd(data);
-      onClose();
+      closeModal();
     } catch (error) {
       console.error('Error creating task:', error.message);
     }
@@ -49,7 +49,7 @@ const AddCardPopup = ({ onClose, onAdd, userId, columnId, accessToken }) => {
     { color: "#8FA1D0", priority: "low" },
     { color: "#E09CB5", priority: "medium" },
     { color: "#BEDBB0", priority: "high" },
-    { color: "#FFFFFF4D", priority: "without" },
+    { color: "#1616164D", priority: "without" },
   ];
 
   const toggleDateInput = () => {
@@ -71,12 +71,17 @@ const AddCardPopup = ({ onClose, onAdd, userId, columnId, accessToken }) => {
   };
 
   return (
+    <div
+    className={s.popupOverlay} onClick={(e) => {
+      if (e.target === e.currentTarget) closeModal();
+    }}
+  >
     <div className={s.popup}>
       <div className={s.popupContent}>
-        <button className={s.closeButton} onClick={onClose}>
+        <button className={s.closeButton} onClick={closeModal}>
           <SvgIcon id="icon-x-close" className={s.svgCloseIcon} width="18" height="18" />
         </button>
-        <label className={s.titleCard}>Add Card</label>
+        <p className={s.TitleCard }>Add сard</p>
         <input
           className={s.inputTitle}
           type="text"
@@ -85,7 +90,7 @@ const AddCardPopup = ({ onClose, onAdd, userId, columnId, accessToken }) => {
           onChange={(e) => setTitle(e.target.value)}
         />
         <textarea
-          className={s.inputTitle}
+          className={s.textareaDeskr}
           placeholder="Description"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
@@ -109,7 +114,7 @@ const AddCardPopup = ({ onClose, onAdd, userId, columnId, accessToken }) => {
         </div>
 
          {/* Використання CalendarPicker для вибору дати */}
-        <div className={s.deadline}>
+        <div className={s.deadlineWrapper}>
           <label className={s.labelTitle}>Deadline</label>
           <div className={s.CalendarPicker}>
             <CalendarPicker
@@ -118,7 +123,7 @@ const AddCardPopup = ({ onClose, onAdd, userId, columnId, accessToken }) => {
               showDateInput={showDateInput}
               toggleDateInput={toggleDateInput}
               formatDate={formatDate}
-              calendarClassName={s.dateDisplay}
+              calendarClassName={s.dateDisplayCalendar}
             />
             <SvgIcon id="icon-chevron-down" className={s.iconChevronDown} width="14" height="14" />
           </div>
@@ -130,125 +135,9 @@ const AddCardPopup = ({ onClose, onAdd, userId, columnId, accessToken }) => {
         </button>
       </div>
     </div>
+  </div>
   );
 };
 
 export default AddCardPopup;
 
-
-
-// import { useState } from 'react';
-// import SvgIcon from '../SvgIcon/SvgIcon';
-// import CalendarPicker from '../CalendarPicker/CalendarPicker';  
-// import s from './AddCardPopup.module.css';
-
-// const AddCardPopup = ({ onClose, onAdd }) => {
-//   const [title, setTitle] = useState('');
-//   const [description, setDescription] = useState('');
-//   const [labelColor, setLabelColor] = useState('');
-//   const [deadline, setDeadline] = useState(new Date());
-//   const [showDateInput, setShowDateInput] = useState(false);
-
-//   const handleAdd = () => {
-//     const newCard = {
-//       title,
-//       description,
-//       labelColor,
-//       deadline: deadline.toISOString().split('T')[0],
-//     };
-//     onAdd(newCard);
-//     onClose();
-//   };
-
-//   const toggleDateInput = () => {
-//     setShowDateInput(!showDateInput);
-//   };
-
-//   const formatDate = (date) => {
-//     const options = { month: 'long', day: 'numeric' };
-//     const today = new Date();
-//     const selectedDate = new Date(date);
-//     if (
-//       selectedDate.getDate() === today.getDate() &&
-//       selectedDate.getMonth() === today.getMonth() &&
-//       selectedDate.getFullYear() === today.getFullYear()
-//     ) {
-//       return `Today, ${selectedDate.toLocaleDateString('en-US', options)}`;
-//     }
-//     return selectedDate.toLocaleDateString('en-US', options);
-//   };
-
-//   return (
-//     <div className={s.popup}>
-//       <div className={s.popupContent}>
-//         <button className={s.closeButton} onClick={onClose}>
-//           <SvgIcon
-//             id="icon-x-close"
-//             className={s.svgCloseIcon}
-//             width="18"
-//             height="18"
-//           />
-//         </button>
-//         <p className={s.TitleCard}>Add Card</p>
-//         <input
-//           className={s.inputTitle}
-//           type="text"
-//           placeholder="Title"
-//           value={title}
-//           onChange={(e) => setTitle(e.target.value)}
-//         />
-//         <textarea
-//           className={s.inputTitle}
-//           placeholder="Description"
-//           value={description}
-//           onChange={(e) => setDescription(e.target.value)}
-//         />
-//         <div className={s.labelColor}>
-//           <p className={s.labelTitle}>Label color</p>
-//           <div>
-//             {['violet', 'pink', 'green', 'black'].map((color) => (
-//               <label key={color}>
-//                 <input
-//                   type="radio"
-//                   name="labelColor"
-//                   value={color}
-//                   onChange={() => setLabelColor(color)}
-//                   checked={labelColor === color}
-//                 />
-//                 <span className={`${s.colorCircle} ${s[color]} ${labelColor === color ? s.active : ''}`} />
-//               </label>
-//             ))}
-//           </div>
-//         </div>
-
-//         {/* Використання нового компонента CalendarPicker */}
-//         <div className={s.deadline}>
-//           <p className={s.labelTitle}>Deadline</p>
-//           <div className={s.CalendarPicker}>
-//           <CalendarPicker
-//             selectedDate={deadline}
-//             onDateChange={setDeadline}
-//             showDateInput={showDateInput}
-//             toggleDateInput={toggleDateInput}
-//             formatDate={formatDate}
-//             calendarClassName={s.dateDisplay}
-//           />
-//           <SvgIcon id="icon-chevron-down" className={s.iconChevronDown} width="14" height="14" />
-//         </div>
-//         </div>
-
-//         <button className={s.addButton} onClick={handleAdd}>
-//           <SvgIcon
-//             id="icon-normalBtnBlack"
-//             className={s.createIcon}
-//             width="28"
-//             height="28"
-//           />
-//           Add
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AddCardPopup;
--- src/components/EditCardPopup/EditCardPopup.jsx
+++ src/components/EditCardPopup/EditCardPopup.jsx
@@ -7,7 +7,7 @@ import CalendarPicker from '../CalendarPicker/CalendarPicker';
 import axios from 'axios';  
 import s from "./EditCardPopup.module.css";
 
-const EditCardPopup = ({ onClose, onEdit, task, userId, columnId, accessToken }) => {
+const EditCardPopup = ({ closeModal, onEdit, task, userId, columnId, accessToken }) => {
   const [title, setTitle] = useState(task.title);
   const [description, setDescription] = useState(task.description);
   const [labelColor, setLabelColor] = useState(task.labelColor);
@@ -40,7 +40,7 @@ const EditCardPopup = ({ onClose, onEdit, task, userId, columnId, accessToken })
 
       if (response.status === 200) {
         onEdit(response.data); 
-        onClose();
+        closeModal();
       } else {
         console.error('Error:', response.statusText);
       }
@@ -53,7 +53,7 @@ const EditCardPopup = ({ onClose, onEdit, task, userId, columnId, accessToken })
     { color: "#8FA1D0", priority: "low" },
     { color: "#E09CB5", priority: "medium" },
     { color: "#BEDBB0", priority: "high" },
-    { color: "#FFFFFF4D", priority: "without" },
+    { color: "#1616164D", priority: "without" },
   ];
 
   const toggleDateInput = () => {
@@ -75,9 +75,14 @@ const EditCardPopup = ({ onClose, onEdit, task, userId, columnId, accessToken })
   };
 
   return (
+    <div
+    className={s.popupOverlay} onClick={(e) => {
+      if (e.target === e.currentTarget) closeModal();
+    }}
+  >
     <div className={s.popup}>
       <div className={s.popupContent}>
-        <button className={s.closeButton} onClick={onClose}>
+        <button className={s.closeButton} onClick={closeModal}>
           <SvgIcon
             id="icon-x-close"
             className={s.svgCloseIcon}
@@ -85,7 +90,7 @@ const EditCardPopup = ({ onClose, onEdit, task, userId, columnId, accessToken })
             height="18"
           />
         </button>
-        <label className={s.TitleCard}>Edit Card</label>
+        <p className={s.TitleCard}>Edit card</p>
         <input
           className={s.inputTitle}
           type="text"
@@ -94,7 +99,7 @@ const EditCardPopup = ({ onClose, onEdit, task, userId, columnId, accessToken })
           onChange={(e) => setTitle(e.target.value)}
         />
         <textarea
-          className={s.inputTitle}
+          className={s.textareaDeskr}
           placeholder="Description"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
@@ -118,7 +123,7 @@ const EditCardPopup = ({ onClose, onEdit, task, userId, columnId, accessToken })
         </div>
 
         {/* Використання CalendarPicker для вибору дати */}
-        <div className={s.deadline}>
+        <div className={s.deadlineWrapper}>
           <label className={s.labelTitle}>Deadline</label>
           <div className={s.CalendarPicker}>
             <CalendarPicker
@@ -144,127 +149,10 @@ const EditCardPopup = ({ onClose, onEdit, task, userId, columnId, accessToken })
         </button>
       </div>
     </div>
+  </div>
+    
   );
 };
 
 export default EditCardPopup;
 
-
-
-
-// import { useState } from 'react';
-// import SvgIcon from '../SvgIcon/SvgIcon';
-// import CalendarPicker from '../CalendarPicker/CalendarPicker'; 
-// import s from "./EditCardPopup.module.css";
-
-// const EditCardPopup = ({ onClose, onEdit, card }) => {
-//   const [title, setTitle] = useState(card.title);
-//   const [description, setDescription] = useState(card.description);
-//   const [labelColor, setLabelColor] = useState(card.labelColor);
-//   const [deadline, setDeadline] = useState(new Date(card.deadline));
-//   const [showDateInput, setShowDateInput] = useState(false);
-
-//   const handleEdit = () => {
-//     const updatedCard = {
-//       ...card,
-//       title,
-//       description,
-//       labelColor,
-//       deadline: deadline.toISOString().split('T')[0],
-//     };
-//     onEdit(updatedCard);
-//     onClose();
-//   };
-
-//   const toggleDateInput = () => {
-//     setShowDateInput(!showDateInput);
-//   };
-
-//   const formatDate = (date) => {
-//     const options = { month: 'long', day: 'numeric' };
-//     const today = new Date();
-//     const selectedDate = new Date(date);
-//     if (
-//       selectedDate.getDate() === today.getDate() &&
-//       selectedDate.getMonth() === today.getMonth() &&
-//       selectedDate.getFullYear() === today.getFullYear()
-//     ) {
-//       return `Today, ${selectedDate.toLocaleDateString('en-US', options)}`;
-//     }
-//     return selectedDate.toLocaleDateString('en-US', options);
-//   };
-
-//   return (
-//     <div className={s.popup}>
-//       <div className={s.popupContent}>
-//         <button className={s.closeButton} onClick={onClose}>
-//           <SvgIcon
-//             id="icon-x-close"
-//             className={s.svgCloseIcon}
-//             width="18"
-//             height="18"
-//           />
-//         </button>
-//         <p className={s.TitleCard}>Edit Card</p>
-//         <input
-//           className={s.inputTitle}
-//           type="text"
-//           placeholder="Title"
-//           value={title}
-//           onChange={(e) => setTitle(e.target.value)}
-//         />
-//         <textarea
-//           className={s.inputTitle}
-//           placeholder="Description"
-//           value={description}
-//           onChange={(e) => setDescription(e.target.value)}
-//         />
-//         <div className={s.labelColor}>
-//           <p className={s.labelTitle}>Label color</p>
-//           <div>
-//             {['violet', 'pink', 'green', 'black'].map((color) => (
-//               <label key={color}>
-//                 <input
-//                   type="radio"
-//                   name="labelColor"
-//                   value={color}
-//                   onChange={() => setLabelColor(color)}
-//                   checked={labelColor === color}
-//                 />
-//                 <span className={`${s.colorCircle} ${s[color]} ${labelColor === color ? s.active : ''}`} />
-//               </label>
-//             ))}
-//           </div>
-//         </div>
-
-//         {/* Using CalendarPicker for date selection */}
-//         <div className={s.deadline}>
-//           <p className={s.labelTitle}>Deadline</p>
-//           <div className={s.CalendarPicker}>
-//           <CalendarPicker
-//             selectedDate={deadline}
-//             onDateChange={setDeadline}
-//             showDateInput={showDateInput}
-//             toggleDateInput={toggleDateInput}
-//             formatDate={formatDate}
-//             calendarClassName={s.dateDisplay}
-//           />
-//           <SvgIcon id="icon-chevron-down" className={s.iconChevronDown} width="14" height="14" />
-//         </div>
-//         </div>
-
-//         <button className={s.addButton} onClick={handleEdit}>
-//           <SvgIcon
-//             id="icon-normalBtnBlack"
-//             className={s.createIcon}
-//             width="28"
-//             height="28"
-//           />
-//           Edit
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default EditCardPopup;
