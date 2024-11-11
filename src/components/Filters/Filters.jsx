da576fd71b66332fad5957ec0f2155f9f8516ced
Vitalii <149200949+Velykobrat@users.noreply.github.com>
2024-11-11 19:33:18 +0200

    Styles for Filters component

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,3 +1,4 @@
+
 import SvgIcon from "../SvgIcon/SvgIcon";
 import css from "./Filters.module.css";
 
@@ -6,33 +7,57 @@ const Filters = ({ onBackgroundChange, onFilterChange, setIsModalOpen }) => {
     setIsModalOpen(false);
   };
 
-  const handleBackgroundChange = (color) => {
+  {/*const handleBackgroundChange = (color) => {
     onBackgroundChange?.(color); 
   };
+  */}
 
   const handleFilterChange = (priority) => {
     onFilterChange?.(priority);
-    toggleModal();
+   // toggleModal(); //закривати модалку при виборі
   };
 
+  
   return (
-    <div className={css.modal}>
-      <div className={css.modalContent}>
+    <div className={css.filters}>
+      <div className={css.filtersContent}>
         <button type="button" className={css.closeButton} onClick={toggleModal}>
           <SvgIcon id="icon-x-close" width="18" height="18" />
         </button>
+
         <h2>Filters</h2>
-        <div>
+
+        {/* Background Options */}
+        {/*
+        <div className={css.backgroundOptions}>
           <h3>Background</h3>
-          <button onClick={() => handleBackgroundChange("default")}>Default Background</button>
-          <button onClick={() => handleBackgroundChange("none")}>Remove Background</button>
+          <button className={css.backgroundButton} onClick={() => handleBackgroundChange("default")}>
+            Default Background
+          </button>
+          <button className={css.backgroundButton} onClick={() => handleBackgroundChange("none")}>
+            Remove Background
+          </button>
         </div>
-        <div>
-          <h3>Label Color</h3>
-          <button onClick={() => handleFilterChange("none")}>Without priority</button>
-          <button onClick={() => handleFilterChange("low")}>Low</button>
-          <button onClick={() => handleFilterChange("medium")}>Medium</button>
-          <button onClick={() => handleFilterChange("high")}>High</button>
+        */}
+
+        
+        {/* Priority Options */}
+        <div className={css.priorityOptions}>
+          <h3>Label color</h3>
+          {["none", "low", "medium", "high"].map((priority) => (
+            <label key={priority} className={css.priorityLabel}>
+              <input 
+                type="radio" 
+                name="priority" 
+                value={priority} 
+                onChange={() => handleFilterChange(priority)} 
+                className={`${css.filterRadio} ${css[`filterRadio--${priority}`]}`} 
+              />
+              <span className={css.radioLabel}>
+                {priority.charAt(0).toUpperCase() + priority.slice(1)}
+              </span>
+            </label>
+          ))}
         </div>
       </div>
     </div>
