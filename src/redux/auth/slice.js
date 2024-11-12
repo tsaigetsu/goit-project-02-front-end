6f00309ed2c1afac3c75d3c4630b3ae7daf62f63
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-11-12 23:20:48 +0100

    fixed modalnee

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -48,6 +48,8 @@ const slice = createSlice({
       .addCase(currentUserThunk.fulfilled, (state, action) => {
         state.isLoggedIn = true;
         state.isRefreshing = false;
+        console.log("currentUserThunk", action.payload);
+
         state.user = action.payload;
       })
       .addCase(currentUserThunk.pending, (state) => {
