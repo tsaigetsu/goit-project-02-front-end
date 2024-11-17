fc9bf82750d330f682521eb5946a69235e4187df
Alina <alia.nord.adventurer@gmail.com>
2024-11-17 17:15:24 +0100

    upd color priority

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -19,10 +19,10 @@ const Card = React.memo(({ card }) => {
   const { columns } = board;
 
   const colorPriority = [
-    { color: ' #8fa1d0', priority: 'low' },
+    { color: '#8fa1d0', priority: 'low' },
     { color: '#E09CB5', priority: 'medium' },
     { color: '#BEDBB0', priority: 'high' },
-    { color: 'rgba(255, 255, 255, 0.3)', priority: 'without priority' },
+    { color: '#B7B7B7', priority: 'without priority' },
   ];
 
   const selectedColorObj = colorPriority.find(
